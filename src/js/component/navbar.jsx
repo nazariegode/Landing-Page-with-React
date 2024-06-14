a2a202d4a899d9342e7c6bcc54656c45dc520f4d
David Nazariego Ulacio <dnazariego@gmail.com>
2024-06-14 18:49:55 +0000

    react

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from "../../img/iconavbar.png"
 
 //create your first component
 
@@ -7,7 +8,7 @@ const Navbar = () => {
 		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 			<div className="container">
 				<a className="navbar-brand" href="#" id="home">
-					<img src="/iconavbar.png" style={{ width: '50px', height: '50px' }} alt="logo" />
+					<img src={logo} style={{ width: '50px', height: '50px' }} alt="logo" />
 				</a>
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
